ee79e0d2e3d716a6991784ffe7a418bb3bbb15bd
sgonzalez <sebastian.gonzalez@gestionartic.com>
2019-05-14 11:04:23 -0500

    Mi primer comentario

---
+++ conexiodb.php
@@ -0,0 +1,10 @@
+<?php 
+
+$serverName = "DESARROLLADOR2";
+$conectionInfo = array("Database" => "biable01", "UID" => "sa", "PWD" => "biable01", "CharacterSet" => "UTF-8");
+$conn = sqlsrv_connect($serverName, $conectionInfo);
+
+
+
+
+ ?>
